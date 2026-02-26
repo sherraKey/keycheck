4b4d0b2b8059964375d3f8c430c7a942a27a8e41
sherraKey <sherraaapremium@gmail.com>
2026-02-26 23:04:00 +0700

    Update Unlidalv.lua

--- Unlidalv.lua
+++ Unlidalv.lua
@@ -3,7 +3,6 @@ local Player = Players.LocalPlayer
 local PlayerGui = Player:WaitForChild("PlayerGui")
 
 -- ================= AUTHORIZATION LOGIC =================
--- ================= AUTHORIZATION LOGIC =================
 local isFullAuthorized = false
 
 local function checkAuthorization()
